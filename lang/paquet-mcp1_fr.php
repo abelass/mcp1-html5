a9327af39dd38de850748df25883c1ee94ced435
abelass <websolutions@mychacra.net>
2012-10-14 17:31:29 +0200

    initialisation

---
+++ lang/paquet-mcp1_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mcp1_description' => 'Pack basique ',
+	'mcp1_nom' => 'My Chacra - Pack basique',
+	'mcp1_slogan' => 'Un premier site',
+);
+
+?>
