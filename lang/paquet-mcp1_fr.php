e0e94f1576d80f0109d665182db91f1c7c7d7201
abelass <websolutions@mychacra.net>
2012-12-01 10:52:38 +0100

    adaptation du titre

--- lang/paquet-mcp1_fr.php
+++ lang/paquet-mcp1_fr.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'mcp1_description' => 'Pack basique ',
-	'mcp1_nom' => 'My Chacra - Pack basique',
-	'mcp1_slogan' => 'Un premier site',
+	'mcp1_description' => 'Pack basique html5',
+	'mcp1_nom' => 'My Chacra - Pack basique html5',
+	'mcp1_slogan' => 'Un premier site en html5',
 );
 
 ?>
